6ef30181fa93085413c5119905998a8616678300
Prathamesh <prathamesh.s0805@gmail.com>
2026-02-07 20:50:27 +0530

    Import pandas and load penguins dataset

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
-
+import pandas as pd
 st.title('🤖 Machine learning app')
 
 st.info('This is a machine learning app')
+df=pd.read('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df=
