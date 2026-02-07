a4100726c067934eb12a1ee0255d3bcbb952f302
Prathamesh <prathamesh.s0805@gmail.com>
2026-02-07 20:51:26 +0530

    Fix CSV read function in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import pandas as pd
 st.title('🤖 Machine learning app')
 
 st.info('This is a machine learning app')
-df=pd.read('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 df
