707cf0c6f99c4a4a73024066a66b2ce6174e03ee
Prathamesh <prathamesh.s0805@gmail.com>
2026-02-08 19:00:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,6 +87,11 @@ clf.fit(x,y)
 ##Apply model to amke predictions
 prediction =clf.predict(input_row)
 prediction_proba=clf.predict_proba(input_row)
-prediction_proba
 
+df_prediction_proba=pd>DataFrame(prediction_proba)
+df_prediction_proba>].columns=['Adelie','Chinstrap','Gentoo']
+df_prediction_proba.rename(columns={0:'Adelie',
+                                   1:'Chinstrap',
+                                   2:'Gentoo'})
+df_prediction_proba
         
