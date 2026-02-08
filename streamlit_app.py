718d5f0365e1da4e07fa51c0e6d76ca30b46b2ba
Prathamesh <prathamesh.s0805@gmail.com>
2026-02-08 19:29:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,10 +95,12 @@ df_prediction_proba.rename(columns={0:'Adelie',
                                    1:'Chinstrap',
                                    2:'Gentoo'})
 
-df_prediction_proba
 
 #display prediciton species
+
 st.subheader('Predicted Species')
+
+df_prediction_proba
 penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
 st.success(str(penguins_species[prediction][0]))
 
