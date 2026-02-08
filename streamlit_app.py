a1273a1b8b23fdf84696ff75cf00d93f189ace69
Prathamesh <prathamesh.s0805@gmail.com>
2026-02-08 13:22:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,13 +11,13 @@ with st.expander('Data'):
     df
 
     st.write('**x**')
-    x=df.drop('species',axis=1)
-    x
+    x_raw=df.drop('species',axis=1)
+    x_raw
 
 
     st.write('**y**')
-    y=df.species
-    y
+    y_raw=df.species
+    y_raw
 
 with st.expander('Data visualtion'):
 
@@ -44,11 +44,21 @@ with st.sidebar:
     input_df=pd.DataFrame(data,index=[0])
     input_penguins=pd.concat([input_df,x],axis=0)
 
+#Encode x
+encode =['island','sex']
+df_penguins =pd.get_dummies(input_penguins,prefix=encode)
+input_row=df_penguins[:1]
 
-    encode =['island','sex']
-    df_penguins =pd.get_dummies(input_penguins,prefix=encode)
-    input_row=df_penguins[:1]
-    
+#Encode y
+target_mapper={
+    'Adelie':0,
+    'chinstrap':1.
+    'Gentoo':2
+}
+def target_encoded(val):
+    return target_mapper[val]
+
+
 
 
 with st.expander('Input Features'):
