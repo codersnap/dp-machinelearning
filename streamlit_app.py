e72ebc82f49511703ec77b2bccd027f49b659ffd
Prathamesh <prathamesh.s0805@gmail.com>
2026-02-08 12:21:21 +0530

    Refactor Streamlit app for car service prediction

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,37 +1,27 @@
 import streamlit as st
 import pandas as pd
-import joblib
 
-# Load model
-model = joblib.load("model/car_service_model.pkl")
+st.tittle('🤖 machine learning APP')
 
-st.title("🚗 Car Service Prediction System")
-st.info("Predict whether a car needs servicing using ML")
+st.info('This is app builds a machine learning model')
 
-km = st.number_input("KM since last service", 0, 20000)
-speed = st.number_input("Average speed (km/h)", 0, 120)
-oil = st.number_input("KM since oil change", 0, 15000)
-brake = st.number_input("Brake pad thickness (mm)", 0.0, 10.0)
-clutch = st.number_input("Clutch pad thickness (mm)", 0.0, 10.0)
-style = st.selectbox("Driving style", ["Smooth", "Aggressive"])
+with st.expand('Data'):
+    st.write('**Raw Data**')
+    df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+    df
 
-style = 0 if style == "Smooth" else 1
+    st.writez('**x**')
+    x=df.drop('species',axis=1)
+    x
 
-if st.button("Predict Service Requirement"):
-    input_data = pd.DataFrame([[km, speed, oil, brake, clutch, style]],
-        columns=[
-            "km_since_last_service",
-            "avg_speed_kmph",
-            "engine_oil_km",
-            "brake_pad_thickness_mm",
-            "clutch_pad_thickness_mm",
-            "driving_style"
-        ]
-    )
 
-    result = model.predict(input_data)[0]
+    st.write('**y**')
+    y=df.species
+    y
 
-    if result == 1:
-        st.error("🔧 Service Required")
-    else:
-        st.success("✅ No Service Needed")
+with st.expand('Data visualtion')
+
+st.scatter_chart(data=df,x='bill_length_mm',y='body_mass_g',color='species')
+
+
+        
