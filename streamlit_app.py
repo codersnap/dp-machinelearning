12804e85e98390bbe177169e27b975ce61ea5d19
Prathamesh <prathamesh.s0805@gmail.com>
2026-02-08 12:52:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ with st.sidebar:
         'island':island,
         'bill_length_mm':bill_length_mm,
         'bill_depth_mm':bill_depth_mm,
-        'flipper_length_mm:flipper_length_mm',
+        'flipper_length_mm':flipper_length_mm,
         'body_mass_g':body_mass_g,
         'gender':gender
          }
